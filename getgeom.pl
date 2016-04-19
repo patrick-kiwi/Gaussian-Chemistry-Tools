35bf4b354d320a491f66759e6dd164ce6e9ab98d
Patrick O'Connor <patrick_kiwi@hotmail.com>
2016-04-19 23:01:39 +1200

    Update getgeom.pl

--- getgeom.pl
+++ getgeom.pl
@@ -2,18 +2,30 @@
 use strict;
 use warnings;
 
-# Extracts charge, multiplicity, and co-ordinates from a log file
-# typical usage ./getgeom.pl foo.log > bar.gjf 
+# Script written by Patrick O'Connor to extract the
+# charge multiplicity and co-ordinates from a log file
+# typical usage ./getgeom foo.log > bar.gjf 
 
+my %data;
 my $counter1=0;
 my $counter2=0;
 my $n_atoms=0;
 my (@symb, @x, @y, @z);
 my ($charge, $multiplicity);
 
+my $filex = glob("$ARGV[0]");
+
 my $in = $ARGV[0];
 	open(my $in_FH, "<", $in) 
 	or die "cant open file $!";
+
+my $out = $in;
+$out =~ s/(.*)\.log$/_NEW_$1.gjf/;
+#print "output file is $out\n";
+
+open(my $out_FH, ">", $out) || die "Cant open $out $!\n";
+
+&load_data($in);
 
 while (<$in_FH>) {
 chomp;
@@ -48,10 +60,41 @@ chomp;
 }
 
   }
+print $out_FH "Geometry generated from $in\n";
+print $out_FH "${$data{$in}}[2]\n\n";
+print $out_FH "\nHF=${$data{$in}}[0]    ZPVE=${$data{$in}}[1]\n";
+print $out_FH "\n$charge $multiplicity\n";
 
-print "\nGeometry generated from $in\n\n$charge $multiplicity\n";
+
 for(my $i = 1; $i <= $n_atoms; $i++) {
-  printf "%s %16.7f %16.7f %16.7f\n",
+  printf $out_FH "%-3s %14.7f %14.7f %14.7f\n",
          $symb[$i],$x[$i], $y[$i], $z[$i];
   }
-print "\n";
+print $out_FH "\n";
+
+
+
+sub load_data {
+my $file =  $in;
+my ($HF, $ZPVE, $fe, $HFZPE, $routeSection);
+my $text = do { local( @ARGV, $/ ) = $file ; <> } ;
+$text =~ s/\n //g;
+foreach ($text =~ m/HF=(-[0-9]+\.[0-9]+)/g) {
+$HF=$1;
+}
+foreach ($text =~ m/ZeroPoint=([0-9]+\.[0-9]+)/g) {
+$ZPVE=$1;
+}
+foreach ($text =~ m/Sum\sof\selectronic\sand\sthermal\sFree\sEnergies=\s+(-[0-9]+\.[0-9]+)/g) {
+$fe=$1;
+}
+foreach ($text =~ m/-{50,200}(#.*?)-{50,200}/) {
+$routeSection = $1;
+}
+
+$HFZPE=$HF+$ZPVE;
+print "Processing $file...\n";
+push (@{$data{$file}}, $HF  );
+push (@{$data{$file}}, $ZPVE);
+push (@{$data{$file}}, $routeSection);
+}
